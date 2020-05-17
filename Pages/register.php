3be385fbcbe3e696554007b3482d519b6d330b39
MathieuEhrsam <ehrsammathieu1@gmail.com>
2020-05-17 16:04:48 +0200

    formulaire inscription + login (non fonctionnel)

---
+++ Pages/register.php
@@ -0,0 +1,10 @@
+<fieldset>
+    <legend>Inscription</legend>
+<form method="post">
+    Votre nom : <input type="text" name="nom">
+    <br />
+    Votre prénom : <input type="text" name="prenom"><br />
+    Votre email : <input type="text" name="email"><br />
+    Votre password : <input type="password" name="pwd"><br />
+    <input type="submit" value="Connexion">
+    </form> </h4></fieldset>
