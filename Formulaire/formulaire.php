1d6c6377b16b38187ecc63fb9d6384e556ff684e
MathieuEhrsam <ehrsammathieu1@gmail.com>
2020-05-17 14:55:29 +0200

    connexion bdd ok

--- Formulaire/formulaire.php
+++ Formulaire/formulaire.php
@@ -1,16 +1,21 @@
 <?php
-try {
+
 $user='root';
-$dsn='mysql:127.0.0.1;dbname=contact;port=3308';
+$dsn='mysql:host=monsite;dbname=contact;';
 $password = "";
 $name= $_POST['name'];
 $email=$_POST['email'];
 $message=$_POST['message'];
 
-$connexion = new PDO($dsn, $user, $password);
-$res=$connexion->query("insert into form (name, email, message) values($name, $email, $message)");
-
+try {
+    $connexion = new PDO($dsn, $user, $password);
+    $req="INSERT INTO form (name, email, message) VALUES (?, ?, ?)";
+    $stmt= $connexion->prepare($req);
+    $stmt->execute([$name, $email, $message]);
+    echo "Ok";
 } catch (PDOException $e) {
     echo 'Connexion échouée : ' . $e->getMessage();
 }
+
 ?>
+
